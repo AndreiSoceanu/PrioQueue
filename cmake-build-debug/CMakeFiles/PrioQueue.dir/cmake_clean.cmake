00c11beedfe92b3532369b9c096c92935a072cc9
Andrei Soceanu <masoceanu@gmail.com>
2021-03-18 20:30:33 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/PrioQueue.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrioQueue.dir/main.c.o"
+  "PrioQueue"
+  "PrioQueue.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PrioQueue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
